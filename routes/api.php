6be8a6d69d9e6454bdc41c37765fb0a447a0602b
Teodora Radovic <tradovic19@gmail.com>
2022-04-29 10:49:57 +0200

    Dodata stranica product list

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,10 @@ Route::post('register',[UserController::class,'register']);
 
 Route::post('login',[UserController::class,'login']);
 
+
+Route::post('addproduct',[ProductController::class,'addProduct']);
+
+
+Route::get('list',[ProductController::class,'list']);
+
+Route::delete('delete/{id}',[ProductController::class,'delete']);
